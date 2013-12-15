c80164b1b31ffe9ad0cec470f3f66f8a8be74dcb
Alan Shaw <alan@freestyle-developments.co.uk>
2013-12-15 18:21:29 +0000

    Reinstate existing tests

--- test/index.js
+++ test/index.js
@@ -12,7 +12,6 @@ var encoding = process.version.indexOf("0.8") == -1 ? {encoding: "utf8"} : "utf8
 describe("markdownpdf", function() {
 
   it("should generate a nonempty PDF from ipsum.md", function (done) {
-
     this.timeout(5000)
 
     tmp.file({postfix: ".pdf"}, function (er, tmpPdfPath, tmpPdfFd) {
@@ -34,41 +33,38 @@ describe("markdownpdf", function() {
   })
 
   it("should call preProcessMd hook", function (done) {
-
     this.timeout(5000)
 
-    var throughCount = 0
-      , preProcessMd = through(function (data) { throughCount++; this.queue(data) })
+    var writeCount = 0
+      , preProcessMd = function () { return through(function (data) { writeCount++; this.queue(data) }) }
 
     markdownpdf({preProcessMd: preProcessMd}).from(__dirname + "/fixtures/ipsum.md").to.string(function (er, pdfStr) {
       assert.ifError(er)
 
       // Test not empty
       assert.ok(pdfStr.length > 0)
-      assert(throughCount > 0, "Through count expected to be > 0")
+      assert(writeCount > 0, "Write count expected to be > 0")
       done()
     })
   })
 
   it("should call preProcessHtml hook", function (done) {
-
     this.timeout(5000)
 
-    var throughCount = 0
-      , preProcessHtml = through(function (data) { throughCount++; this.queue(data) })
+    var writeCount = 0
+      , preProcessHtml = function () { return through(function (data) { writeCount++; this.queue(data) }) }
 
     markdownpdf({preProcessHtml: preProcessHtml}).from(__dirname + "/fixtures/ipsum.md").to.string(function (er, pdfStr) {
       assert.ifError(er)
 
       // Test not empty
       assert.ok(pdfStr.length > 0)
-      assert(throughCount > 0, "Through count expected to be > 0")
+      assert(writeCount > 0, "Write count expected to be > 0")
       done()
     })
   })
-/*
-  it("should concatenate source files", function (done) {
 
+  it("should concatenate source files", function (done) {
     this.timeout(5000)
 
     var files = [
@@ -76,25 +72,25 @@ describe("markdownpdf", function() {
       , __dirname + "/fixtures/second.md"
     ]
 
-    markdownpdf(files, {concatFiles: true}, function(er, pdf) {
+    tmp.file({postfix: ".pdf"}, function (er, tmpPdfPath, tmpPdfFd) {
       assert.ifError(er)
+      fs.close(tmpPdfFd)
 
-      // Only one PDF should be created
-      if (Object.prototype.toString.call(pdf) == '[object Array]')
-        throw "concatenated source files should return one pdf"
-
-      // Read the file
-      fs.readFile(pdf, {encoding: "utf-8"}, function (er, data) {
+      markdownpdf().concat.from(files).to(tmpPdfPath, function (er) {
         assert.ifError(er)
-        // Test not empty
-        assert.ok(data.length > 0)
-        done()
+
+        // Read the file
+        fs.readFile(tmpPdfPath, encoding, function (er, data) {
+          assert.ifError(er)
+          // Test not empty
+          assert.ok(data.length > 0)
+          done()
+        })
       })
     })
   })
 
-  it("should return array of paths when converting multiple files", function (done) {
-
+  it("should write to multiple paths when converting multiple files", function (done) {
     this.timeout(5000)
 
     var files = [
@@ -102,26 +98,29 @@ describe("markdownpdf", function() {
       , __dirname + "/fixtures/second.md"
     ]
 
-    markdownpdf(files, function(er, pdfs) {
+    tmp.file({postfix: ".pdf"}, function (er, tmpPdfPath0, tmpPdfFd0) {
       assert.ifError(er)
+      fs.close(tmpPdfFd0)
 
-      // Two PDFs should be created
-      if (Object.prototype.toString.call(pdfs) == '[object String]')
-        throw "converting multiple files should return multiple paths"
+      tmp.file({postfix: ".pdf"}, function (er, tmpPdfPath1, tmpPdfFd1) {
+        assert.ifError(er)
+        fs.close(tmpPdfFd1)
 
-      assert.equal(pdfs.length, 2, "should return two file paths")
-
-      // Read the files
-      for (var i = 0; i < pdfs.length; i++) {
-        fs.readFile(pdfs[i], {encoding: "utf-8"}, function (er, data) {
+        markdownpdf().from.paths(files).to.paths([tmpPdfPath0, tmpPdfPath1], function (er) {
           assert.ifError(er)
-          // Test not empty
-          assert.ok(data.length > 0)
-        })
-      }
 
-      done()
+          // Read the file
+          var content0 = fs.readFileSync(tmpPdfPath0, encoding)
+          var content1 = fs.readFileSync(tmpPdfPath1, encoding)
+
+          assert.ok(content0.length > 0)
+          assert.ok(content1.length > 0)
+          assert.ok(content0.length != content1.length)
+
+          done()
+        })
+      })
     })
   })
-*/
+
 })
