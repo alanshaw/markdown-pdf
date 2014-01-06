79819ec33ff0629c92d54a7cc92cb28eb58ebae9
Falco Nogatz <fnogatz@gmail.com>
2014-01-06 13:22:55 +0100

    added page header and footer option; Resolves #12

--- test/index.js
+++ test/index.js
@@ -3,6 +3,7 @@ var markdownpdf = require("../")
   , fs = require("fs")
   , tmp = require("tmp")
   , through = require("through")
+  , pdfText = require("pdf-text")
 
 tmp.setGracefulCleanup()
 
@@ -24,6 +25,35 @@ describe("markdownpdf", function() {
           // Test not empty
           assert.ok(data.length > 0)
           done()
+        })
+      })
+    })
+  })
+
+  it("should have a header and footer", function (done) {
+    this.timeout(5000)
+
+    tmp.file({postfix: ".pdf"}, function (er, tmpPdfPath, tmpPdfFd) {
+      assert.ifError(er)
+      fs.close(tmpPdfFd)
+
+      markdownpdf({runningsPath: __dirname+'/fixtures/runnings.js'}).from(__dirname + "/fixtures/ipsum.md").to(tmpPdfPath, function (er) {
+        assert.ifError(er)
+
+        // Read the file
+        fs.readFile(tmpPdfPath, {encoding: "utf8"}, function (er, data) {
+          assert.ifError(er)
+          // Test not empty
+          assert.ok(data.length > 0)
+
+          // Header and footer included?
+          pdfText(tmpPdfPath, function (er, chunks) {
+            assert.ifError(er)
+
+            assert.ok(/Some\s?Header/.test(chunks.join('')))
+            assert.ok(/Some\s?Footer/.test(chunks.join('')))
+            done()
+          })
         })
       })
     })
