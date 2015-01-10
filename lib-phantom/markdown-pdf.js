c4b8cb4d408fc635c45c370dc6e91407af4f2f00
Alan Shaw <alan@tableflip.io>
2015-01-10 15:13:33 +0000

    fixes #39 fixes #35 fixes #21 Images in markdown documents now loaded from --cwd option or process.cwd() if not set

--- lib-phantom/markdown-pdf.js
+++ lib-phantom/markdown-pdf.js
@@ -3,64 +3,43 @@ var system = require("system")
   , fs = require("fs")
 
 // Read in arguments
-var args = ["in", "out", "runningsPath", "cssPath", "highlightCssPath", "paperFormat", "paperOrientation", "paperBorder", "renderDelay", "jsonPath"].reduce(function (args, name, i) {
+var args = ["in", "out", "cwd", "runningsPath", "cssPath", "highlightCssPath", "paperFormat", "paperOrientation", "paperBorder", "renderDelay", "jsonPath"].reduce(function (args, name, i) {
   args[name] = system.args[i + 1]
   return args
 }, {})
 
-page.open(page.libraryPath + "/../html5bp/index.html", function (status) {
-  
-  if (status == "fail") {
-    page.close()
-    phantom.exit(1)
-    return
-  }
-  
-  // Add custom CSS to the page
-  page.evaluate(function(cssPaths) {
-    
-    var head = document.querySelector("head")
-    
-    cssPaths.forEach(function(cssPath) {
-      
-      var css = document.createElement("link")
-      
-      css.rel = "stylesheet"
-      css.href = cssPath
-      
-      head.appendChild(css)
+var html5bpPath = page.libraryPath + "/../html5bp"
 
-    });
-    
-  }, [args.cssPath, args.highlightCssPath])
-  
-  // Add the HTML to the page
-  page.evaluate(function(html) {
-    
-    var body = document.querySelector("body")
+var html = fs.read(html5bpPath + "/index.html")
+  .replace(/\{\{baseUrl\}\}/g, "file://" + html5bpPath)
+  .replace("{{content}}", fs.read(args.in))
 
-    // Remove the paragraph HTML5 boilerplate adds
-    body.removeChild(document.querySelector("p"))
-    
-    var container = document.createElement("div")
-    container.innerHTML = html
-    
-    body.appendChild(container)
-    
-  }, fs.read(args.in))
-  
-  // Set the PDF paper size
-  page.paperSize = paperSize(args.runningsPath, {format: args.paperFormat, orientation: args.paperOrientation, border: args.paperBorder})
+page.setContent(html, "file://" + args.cwd + "/markdown-pdf.html")
 
-  // Render the page
-  setTimeout(function () {
-    page.render(args.out)
-    page.close()
-    phantom.exit(0)
-  }, parseInt(args.renderDelay, 10))
-})
+// Add custom CSS to the page
+page.evaluate(function (cssPaths) {
+  var head = document.querySelector("head")
 
-function paperSize(runningsPath, obj) {
+  cssPaths.forEach(function (cssPath) {
+    var css = document.createElement("link")
+    css.rel = "stylesheet"
+    css.href = cssPath
+
+    head.appendChild(css)
+  })
+}, [args.cssPath, args.highlightCssPath])
+
+// Set the PDF paper size
+page.paperSize = paperSize(args.runningsPath, {format: args.paperFormat, orientation: args.paperOrientation, border: args.paperBorder})
+
+// Render the page
+setTimeout(function () {
+  page.render(args.out)
+  page.close()
+  phantom.exit(0)
+}, parseInt(args.renderDelay, 10))
+
+function paperSize (runningsPath, obj) {
   var runnings = require(runningsPath)
 
   // encapsulate .contents into phantom.callback()
